6d3e8870985877156ee57ec3620a5e9ecc3cfce2
Padmapriyavk <priyahereplus@gmail.com>
2025-08-22 13:37:37 +0530

    Add files via upload
    Day1Assignment

---
+++ day1assignment/Firefox.java
@@ -0,0 +1,17 @@
+package week1.day1assignment;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         float browserVersion = 100.2f;
+	     String browserName = "firefox";
+		 boolean isVisible = true;
+		 short releaseYear = 1998;
+		 char browserLogo = 'f';
+				 System.out.print(browserVersion+"\n"+browserName+"\n"+isVisible+"\n"+releaseYear+"\n"+browserLogo);
+	System.out.print("Check");
+	}
+	
+
+}
